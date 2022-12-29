0edc8fdb56caa189f1c00ce363502f7c3f1cca22
lmw <alsdndjdrktl@gmail.com>
2022-12-29 20:06:23 +0900

    feat(#2): upbit api 커스텀 훅 구현
    - 마켓 API 조회 커스텀 훅
    - 원화/BTC/USDT jotai 상태 저장

---
+++ src/stores/atoms/markets.ts
@@ -0,0 +1,24 @@
+import { atom } from 'jotai'
+import { selectAtom } from 'jotai/utils'
+import deepEquals from 'fast-deep-equal'
+import { Market } from '~/types/api/upbit/market'
+
+export const marketsAtom = atom<Market[]>([])
+
+export const krwMarketsAtom = selectAtom(
+ marketsAtom,
+ (markets) => markets.filter(({ market }) => market.startsWith('KRW-')),
+ deepEquals,
+)
+
+export const btcMarketsAtom = selectAtom(
+ marketsAtom,
+ (markets) => markets.filter(({ market }) => market.startsWith('BTC-')),
+ deepEquals,
+)
+
+export const usdtMarketsAtom = selectAtom(
+ marketsAtom,
+ (markets) => markets.filter(({ market }) => market.startsWith('USDT-')),
+ deepEquals,
+)
