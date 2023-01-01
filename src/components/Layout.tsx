c7b41a019de70382532665d7d3c028f3cc024756
lmw <alsdndjdrktl@gmail.com>
2023-01-01 11:19:12 +0900

    feat(#6): Header 컴포넌트 그리고 Firebase 로그인 연동
    - NavLink, 스타일 및 아이콘 적용

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,12 +1,86 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
+import Gnb from '~/components/ui/Gnb/gnb'
 
 function Layout() {
  return (
-  <div className="flex flex-col min-h-screen">
-   <header>header</header>
-   <main className="grow bg-slate-600 min-h-[550px]">
+  <div className="flex flex-col min-h-screen w-full min-w-[1400px]">
+   <Gnb />
+   <main className="grow min-h-[550px] px-4">
     <Outlet />
+    <p>main</p>
+    <p>main</p>
+    <p>main</p>
+    <p>main</p>
+    <p>main</p>
+    <p>main</p>
+    <p>main</p>
+    <p>main</p>
+    <p>main</p>
+    <p>main</p>
+    <p>main</p>
+    <p>main</p>
+    <p>main</p>
+    <p>main</p>
+    <p>main</p>
+    <p>main</p>
+    <p>main</p>
+    <p>main</p>
+    <p>main</p>
+    <p>main</p>
+    <p>main</p>
+    <p>main</p>
+    <p>main</p>
+    <p>main</p>
+    <p>main</p>
+    <p>main</p>
+    <p>main</p>
+    <p>main</p>
+    <p>main</p>
+    <p>main</p>
+    <p>main</p>
+    <p>main</p>
+    <p>main</p>
+    <p>main</p>
+    <p>main</p>
+    <p>main</p>
+    <p>main</p>
+    <p>main</p>
+    <p>main</p>
+    <p>main</p>
+    <p>main</p>
+    <p>main</p>
+    <p>main</p>
+    <p>main</p>
+    <p>main</p>
+    <p>main</p>
+    <p>main</p>
+    <p>main</p>
+    <p>main</p>
+    <p>main</p>
+    <p>main</p>
+    <p>main</p>
+    <p>main</p>
+    <p>main</p>
+    <p>main</p>
+    <p>main</p>
+    <p>main</p>
+    <p>main</p>
+    <p>main</p>
+    <p>main</p>
+    <p>main</p>
+    <p>main</p>
+    <p>main</p>
+    <p>main</p>
+    <p>main</p>
+    <p>main</p>
+    <p>main</p>
+    <p>main</p>
+    <p>main</p>
+    <p>main</p>
+    <p>main</p>
+    <p>main</p>
+    <p>main</p>
    </main>
    <footer>footer</footer>
   </div>
