af084bfead4aa7c78dac7f61617e40de0cc5b266
lmw <alsdndjdrktl@gmail.com>
2023-01-01 16:07:42 +0900

    feat(#8): 가상 스크롤 컴포넌트 구현
    - transform을 이용해 repain/reflow를 방지하여 성능 최적화

---
+++ src/components/ui/Common/VirtualScroll/virtualScroll.tsx
@@ -0,0 +1,35 @@
+import React from 'react'
+
+interface Props {
+ height: number
+ itemHeight: number
+ gap?: number
+ children: React.ReactNode[]
+ offsetY: number
+ renderAhead?: number
+}
+
+function VirtualScroll(props: Props) {
+ const { height, itemHeight, gap = 0, children, offsetY, renderAhead = 0 } = props
+ const scrollRef = React.useRef<HTMLDivElement>(null)
+
+ const containerHeight = (itemHeight + gap) * children.length
+ const startOffset = Math.max(Math.floor(offsetY / (itemHeight + gap)) - renderAhead, 0)
+ const endOffset = Math.min(Math.ceil(height / (itemHeight + gap) + startOffset) + renderAhead, children.length)
+ const renderJsx = children.slice(Math.max(startOffset, 0), Math.min(endOffset + 1, children.length))
+ const translateY = Math.max((itemHeight + gap) * startOffset, gap)
+
+ return (
+  <div
+   className="will-change-transform"
+   style={{
+    height: `${containerHeight}px`,
+   }}
+   ref={scrollRef}
+  >
+   <div style={{ transform: `translateY(${translateY}px)` }}>{renderJsx}</div>
+  </div>
+ )
+}
+
+export default React.memo(VirtualScroll)
